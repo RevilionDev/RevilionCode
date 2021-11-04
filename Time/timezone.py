65aaf97e1155da46867e2c281f1b0879802c15cd
unknown <damodaraswarupa@gmail.com>
2021-11-04 19:39:52 +0800

    first commit

---
+++ Time/timezone.py
@@ -0,0 +1,6 @@
+import pytz
+from datetime import datetime
+
+take_tokyo = datetime.now(pytz.timezone('Asia/Tokyo')) # https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568
+tokyo = take_tokyo.strftime("%H:%M:%S")
+print('Tokyo Time is', tokyo)
