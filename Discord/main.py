60b7c5e26d0f6d27cfd40a508f5b94d3ce27f68b
unknown <damodaraswarupa@gmail.com>
2021-11-04 20:07:31 +0800

    Black formatting

--- Discord/main.py
+++ Discord/main.py
@@ -3,6 +3,12 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="-", intents=discord.Intents.all())
+bot.remove_command("help")
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} Is ready.")
 
 
 @bot.command()
