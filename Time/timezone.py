4c64eaa6c37750878cf401aaeb0ddb31df96e658
unknown <damodaraswarupa@gmail.com>
2021-11-04 20:11:10 +0800

    Black formatting

--- Time/timezone.py
+++ Time/timezone.py
@@ -4,5 +4,6 @@ from datetime import datetime
 take_tokyo = datetime.now(
     pytz.timezone("Asia/Tokyo")
 )  # https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568
+
 tokyo = take_tokyo.strftime("%H:%M:%S")
 print("Tokyo Time is", tokyo)
