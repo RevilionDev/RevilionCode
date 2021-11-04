2e1916fb88b1fa3edff7296846ef4e57494aae63
unknown <damodaraswarupa@gmail.com>
2021-11-04 19:53:16 +0800

    Black formatting

--- Discord/main.py
+++ Discord/main.py
@@ -1,1 +1,15 @@
-# Maybe later
+# Remember this is just simple setup!
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="-", intents=discord.Intents.all())
+
+
+@bot.command()
+async def hi(ctx):
+    await ctx.send("Hi there!")
+
+
+bot.run(
+    "TOKEN"
+)  # To get Bot Token, you need go to https://discord.com/developers/applications
