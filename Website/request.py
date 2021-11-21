2db175d8cea159efa93f2fcd5660b7fb5c3bf4d8
unknown <damodaraswarupa@gmail.com>
2021-11-21 11:36:48 +0800

    Added Website Examples

---
+++ Website/request.py
@@ -0,0 +1,7 @@
+import requests
+from requests import get
+import json
+
+
+# Print Status of a Website
+print(requests.get('https://github.com/RevilionDev/RevilionCode')) # Output <Response [200]> If website is UP
