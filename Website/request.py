63184753d63b1b2a0b221613f384eded50888fc9
unknown <damodaraswarupa@gmail.com>
2021-11-21 11:43:49 +0800

    Add json requests examples

--- Website/request.py
+++ Website/request.py
@@ -5,3 +5,6 @@ import json
 
 # Print Status of a Website
 print(requests.get('https://github.com/RevilionDev/RevilionCode')) # Output <Response [200]> If website is UP
+
+json_web = requests.get('https://raw.githubusercontent.com/RevilionDev/RevilionCode/main/Website/jsonexample.json').json()
+print(json_web["RevilionDev"]) # The output will be "Cool!"
